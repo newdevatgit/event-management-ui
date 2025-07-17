ce9d945abf3e6b76327efeeeb60123ca7d284f98
newdevatgit <arpanwar26@gmail.com>
2025-07-17 15:01:33 +0530

    new fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ export default {
     extend: {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
+        script: ['Dancing Script', 'cursive'],
+        satisfy: ['Satisfy', 'cursive'],
       },
     },
   },
